8a6d9489b343049fabab7fc8c44bcb6d76a2091d
xiri25 <pokesaidesdios@hotmail.com>
2025-07-10 03:48:12 +0200

    fix config

--- .config/nvim/lua/josean/plugins/lsp/lspconfig.lua
+++ .config/nvim/lua/josean/plugins/lsp/lspconfig.lua
@@ -7,14 +7,8 @@ return {
     { "folke/neodev.nvim", opts = {} },
   },
   config = function()
-    -- import lspconfig plugin
-    local lspconfig = require("lspconfig")
-
     -- import mason_lspconfig plugin
     local mason_lspconfig = require("mason-lspconfig")
-
-    -- import cmp-nvim-lsp plugin
-    local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
     local keymap = vim.keymap -- for conciseness
 
@@ -27,19 +21,19 @@ return {
 
         -- set keybinds
         opts.desc = "Show LSP references"
-        keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
+        keymap.set("n", "gR", vim.lsp.buf.references, opts) -- show definition, references
 
         opts.desc = "Go to declaration"
         keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
 
         opts.desc = "Show LSP definitions"
-        keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
+        keymap.set("n", "gd", vim.lsp.buf.definition, opts) -- show lsp definitions
 
         opts.desc = "Show LSP implementations"
-        keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
+        keymap.set("n", "gi", vim.lsp.buf.implementation, opts) -- show lsp implementations
 
         opts.desc = "Show LSP type definitions"
-        keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
+        keymap.set("n", "gt", vim.lsp.buf.type_definition, opts) -- show lsp type definitions
 
         opts.desc = "See available code actions"
         keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
@@ -47,8 +41,9 @@ return {
         opts.desc = "Smart rename"
         keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
 
-        opts.desc = "Show buffer diagnostics"
-        keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
+        -- TODO: No se
+        -- opts.desc = "Show buffer diagnostics"
+        -- keymap.set("n", "<leader>D", vim.lsp.buf.diagnostics, opts) -- show  diagnostics for file
 
         opts.desc = "Show line diagnostics"
         keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
@@ -67,68 +62,57 @@ return {
 
         opts.desc = "Signature Help"
         keymap.set("n", "<C-h>", function () vim.lsp.buf.signature_help() end, opts)
-        -- keymap.set("n", "<C-h>", function () print("Hoal") end, opts)
       end,
     })
 
-    -- used to enable autocompletion (assign to every lsp server config)
-    local capabilities = cmp_nvim_lsp.default_capabilities()
-
-    -- Change the Diagnostic symbols in the sign column (gutter)
-    -- (not in youtube nvim video)
-    local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
-    for type, icon in pairs(signs) do
-      local hl = "DiagnosticSign" .. type
-      vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-    end
-
-    mason_lspconfig.setup_handlers({
+    mason_lspconfig.setup {}
+--     mason_lspconfig.setup_handlers({
       -- default handler for installed servers
-      function(server_name)
-        lspconfig[server_name].setup({
-          capabilities = capabilities,
-        })
-      end,
-      ["lua_ls"] = function()
-        -- configure lua server (with special settings)
-        lspconfig["lua_ls"].setup({
-          capabilities = capabilities,
-          settings = {
-            Lua = {
-              -- make the language server recognize "vim" global
-              diagnostics = {
-                globals = { "vim" },
-              },
-              completion = {
-                callSnippet = "Replace",
-              },
-            },
-          },
-        })
-      end,
-      ["clangd"] = function()
-        lspconfig["clangd"].setup({
-          capabilities = capabilities,
-          cmd = {
-            "clangd",
-            "--all-scopes-completion",
-            "--background-index",
-            "--clang-tidy",
-            "--completion-parse=always",
-            "--completion-style=bundled",
-            "--cross-file-rename",
-            "--debug-origin",
-            "--enable-config", -- clangd 11+ supports reading from .clangd configuration file
-            "--fallback-style=Qt",
-            "--folding-ranges",
-            "--function-arg-placeholders",
-            "--header-insertion=iwyu",
-            "--pch-storage=memory", -- could also be disk
-            "--suggest-missing-includes",
-          },
-        })
-      end,
-    })
+--     function(server_name)
+--        lspconfig[server_name].setup({
+--          capabilities = capabilities,
+--        })
+--      end,
+--      ["lua_ls"] = function()
+--        -- configure lua server (with special settings)
+--        lspconfig["lua_ls"].setup({
+--          capabilities = capabilities,
+--          settings = {
+--            Lua = {
+--              -- make the language server recognize "vim" global
+--              diagnostics = {
+--                globals = { "vim" },
+--              },
+--              completion = {
+--                callSnippet = "Replace",
+--              },
+--            },
+--          },
+--        })
+--      end,
+--      ["clangd"] = function()
+--        lspconfig["clangd"].setup({
+--          capabilities = capabilities,
+--          cmd = {
+--            "clangd",
+--            "--all-scopes-completion",
+--            "--background-index",
+--            "--clang-tidy",
+--            "--completion-parse=always",
+--            "--completion-style=bundled",
+--            "--cross-file-rename",
+--            "--debug-origin",
+--            "--enable-config", -- clangd 11+ supports reading from .clangd configuration file
+--            "--fallback-style=Qt",
+--            "--folding-ranges",
+--            "--function-arg-placeholders",
+--            "--header-insertion=iwyu",
+--            "--pch-storage=memory", -- could also be disk
+--            "--suggest-missing-includes",
+--          },
+--        })
+--      end,
+--    })
   end,
 }
 
