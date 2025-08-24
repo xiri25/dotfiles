a0450db9ce90b1373a844938fa1bb5c07f0eea67
xiri25 <pokesaidesdios@hotmail.com>
2025-08-24 09:47:04 +0200

    No need for that comment

--- .config/nvim/lua/josean/core/options.lua
+++ .config/nvim/lua/josean/core/options.lua
@@ -17,8 +17,6 @@ opt.wrap = false
 opt.ignorecase = true -- ignore case when searching
 opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
 
--- turn on termguicolors for tokyonight colorscheme to work
--- (have to use iterm2 or any other true color terminal)
 opt.termguicolors = true
 opt.background = "dark" -- colorschemes that can be light or dark will be made dark
 opt.signcolumn = "yes" -- show sign column so that text doesn't shift
