4517991705fb95724731601601085fa18f3022cd
xiri25 <pokesaidesdios@hotmail.com>
2025-10-01 13:30:26 +0200

    Ops, solo te tienes que activar cuando es un archivo de python

--- .config/nvim/lsp/pylsp.lua
+++ .config/nvim/lsp/pylsp.lua
@@ -1,5 +1,14 @@
 return {
   cmd = {'pylsp'},
+  filetypes = { 'python' },
+  root_markers = {
+    'pyproject.toml',
+    'setup.py',
+    'setup.cfg',
+    'requirements.txt',
+    'Pipfile',
+    '.git',
+  },
   settings = {
     pylsp = {
       plugins = {
