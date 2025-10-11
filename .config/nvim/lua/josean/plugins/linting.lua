afa637975c8979955a9d46d2a718751300795607
xiri25 <pokesaidesdios@hotmail.com>
2025-10-11 18:23:56 +0200

    -nvim-cmp +blin-cmp

--- .config/nvim/lua/josean/plugins/linting.lua
+++ .config/nvim/lua/josean/plugins/linting.lua
@@ -1,16 +1,14 @@
 return {
+-- TODO: IDK how it works
+-- TODO: IT needs some fixing to work with python
 --  "mfussenegger/nvim-lint",
 --  event = { "BufReadPre", "BufNewFile" },
 --  config = function()
 --    local lint = require("lint")
 --
 --    lint.linters_by_ft = {
---      javascript = { "eslint_d" },
---      typescript = { "eslint_d" },
---      javascriptreact = { "eslint_d" },
---      typescriptreact = { "eslint_d" },
---      svelte = { "eslint_d" },
 --      python = { "pylint" },
+--      clang = { "clangtify" },
 --    }
 --
 --    local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
@@ -24,6 +22,7 @@ return {
 --
 --    vim.keymap.set("n", "<leader>l", function()
 --      lint.try_lint()
+--      print("linting")
 --    end, { desc = "Trigger linting for current file" })
 --  end,
 }
