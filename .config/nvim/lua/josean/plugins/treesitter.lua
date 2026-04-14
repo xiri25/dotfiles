45e2a4d8311739f09f4a74296a72ef564d1613a1
xiri25 <pokesaidesdios@hotmail.com>
2026-04-14 15:16:10 +0200

    treesitter fix, telescope fix que creo que tenia que ver con treesitter

--- .config/nvim/lua/josean/plugins/treesitter.lua
+++ .config/nvim/lua/josean/plugins/treesitter.lua
@@ -1,52 +1,61 @@
+-- Instalar como dependencia el paquete tree-sitter-cli
 return {
-  "nvim-treesitter/nvim-treesitter",
-  branch = 'main',
-  event = { "BufReadPre", "BufNewFile" },
-  build = ":TSUpdate",
-  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    lazy = false,
+    build = ":TSUpdate",
+    branch = 'main',
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
     "windwp/nvim-ts-autotag",
-  },
---  config = function()
---    -- import nvim-treesitter plugin
---    local treesitter = require("nvim-treesitter.configs")
---
---    -- configure treesitter
---    treesitter.setup({ -- enable syntax highlighting
---      highlight = {
---        enable = true,
---      },
---      -- enable indentation
---      indent = { enable = true },
---      -- enable autotagging (w/ nvim-ts-autotag plugin)
---      autotag = {
---        enable = true,
---      },
---      -- ensure these language parsers are installed
---      ensure_installed = {
---        "json",
---        "yaml",
---        "html",
---        "css",
---        "markdown",
---        "markdown_inline",
---        "bash",
---        "lua",
---        "vim",
---        "dockerfile",
---        "gitignore",
---        "vimdoc",
---        "c",
---        "python",
---      },
---      incremental_selection = {
---        enable = true,
---        keymaps = {
---          init_selection = "<C-space>",
---          node_incremental = "<C-space>",
---          scope_incremental = false,
---          node_decremental = "<bs>",
---        },
---      },
---    })
---  end,
+    },
+    config = function()
+        local ts = require 'nvim-treesitter'
+        local parsers = {
+            "asm",
+            "bash",
+            "c",
+            "cpp",
+            "diff",
+            "dockerfile",
+            "git_config",
+            "gitcommit",
+            "gitignore",
+            "html",
+            "lua",
+            "make",
+            "markdown",
+            "python",
+            "ssh_config",
+            "sql",
+            "toml",
+            "vim",
+            "vimdoc",
+            "yaml",
+        }
+
+        for _, parser in ipairs(parsers) do
+            ts.install(parser)
+        end
+
+        -- Not every tree-sitter parser is the same as the file type detected
+        -- So the patterns need to be registered more cleverly
+        local patterns = {}
+        for _, parser in ipairs(parsers) do
+            local parser_patterns = vim.treesitter.language.get_filetypes(parser)
+            for _, pp in pairs(parser_patterns) do
+                table.insert(patterns, pp)
+            end
+        end
+
+        vim.treesitter.language.register("groovy", "Jenkinsfile")
+        vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+        vim.wo[0][0].foldmethod = 'expr'
+
+        vim.api.nvim_create_autocmd('FileType', {
+            pattern = patterns,
+            callback = function()
+                vim.treesitter.start()
+            end,
+        })
+    end,
 }
